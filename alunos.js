2aa05cd005a57042b704d63a73e8f6f4379837d5
gabisoarezc <gabriela.soares_@hotmail.com>
2021-10-08 18:48:34 -0300

    Fazendo meu primeiro commit na mão com as alunas do construdelas

---
+++ alunos.js
@@ -0,0 +1,78 @@
+nomeAluno1 = prompt ("Nome do aluno 1:")
+nota1Aluno1 = parseInt(prompt("Nota 1:"));
+nota2Aluno1 = parseInt(prompt("Nota 2:"));
+nota3Aluno1 = parseInt(prompt("Nota 3:"));
+nota1= (nota1Aluno1 + "," + nota2Aluno1 + "," + nota3Aluno1)
+media1=(nota1Aluno1 + nota2Aluno1+ nota3Aluno1 )/3
+alert (" Aluno (a) " + nomeAluno1 + " teve as notas " + nota1)
+
+nomeAluno2 = prompt ("Nome do aluno 2:")
+nota1Aluno2 = parseInt(prompt("Nota 1:"));
+nota2Aluno2 = parseInt(prompt("Nota 2:"));
+nota3Aluno2 = parseInt(prompt("Nota 3:"));
+nota2=(nota1Aluno2 + "," + nota2Aluno2 + "," + nota3Aluno2)
+media2=(nota1Aluno2 + nota2Aluno2 + nota3Aluno2 )/3
+alert (" Aluno (a) " + nomeAluno2 + " teve as notas " + nota2)
+
+nomeAluno3 = prompt ("Nome do aluno 3:")
+nota1Aluno3 = parseInt(prompt("Nota 1:"));
+nota2Aluno3 = parseInt(prompt("Nota 2:"));
+nota3Aluno3 =parseInt(prompt("Nota 3:"));
+nota3 = (nota1Aluno3 + "," + nota2Aluno3 + "," + nota3Aluno3)
+media3=(nota1Aluno3 + nota2Aluno3 + nota3Aluno3 )/3
+alert (" Aluno (a) " + nomeAluno3 + " teve as notas " + nota3)
+
+nomeAluno4 = prompt ("Nome do aluno 4:")
+nota1Aluno4 = parseInt(prompt("Nota 1:"));
+nota2Aluno4 = parseInt(prompt("Nota 2:"));
+nota3Aluno4 =parseInt(prompt("Nota 3:"));
+nota4 = (nota1Aluno4 + "," + nota2Aluno4 + "," + nota3Aluno4 )
+media4=(nota1Aluno4 + nota2Aluno4 + nota3Aluno4 )/3
+alert (" Aluno (a) " + nomeAluno4 + " teve as notas " + nota4)
+
+nomeAluno5 = prompt ("Nome do aluno 5:")
+nota1Aluno5 = parseInt(prompt("Nota 1:"));
+nota2Aluno5 = parseInt(prompt("Nota 2:"));
+nota3Aluno5 =parseInt(prompt("Nota 3:"));
+nota5 = (nota1Aluno5 + "," + nota2Aluno5 + "," + nota3Aluno5)
+media5=(nota1Aluno5 + nota2Aluno5 + nota3Aluno5 )/3
+alert (" Aluno (a) " + nomeAluno5 + " teve as notas " + nota5)
+
+alert (" Aluno(a) " + nomeAluno1 + " teve as notas " + nota1)
+
+if (media1 >= 5) {
+  alert ("Aprovado")
+}else {
+  alert ("Reprovado")
+}
+
+alert ("Aluno(a)" +nomeAluno2 + " teve as notas " +nota2)
+
+if (media2 >= 5) {
+  alert ("Aprovado")
+} else {
+  alert ("Reprovado")
+}
+
+alert ("Aluno(a)" +nomeAluno3 + " teve as notas " +nota3)
+
+if (media3 >= 5) {
+  alert ("Aprovado")
+} else {
+  alert ("Reprovado")
+}
+
+alert ("Aluno(a)" +nomeAluno4 + " teve as notas " +nota4)
+
+if (media4 >= 5) {
+  alert ("Aprovado")
+} else {
+  alert ("Reprovado")
+}
+
+alert ("Aluno(a)" +nomeAluno5 + " teve as notas " +nota5)
+if (media5 >= 5) {
+  alert ("Aprovado")
+} else {
+  alert ("Reprovado")
+}
